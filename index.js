eb07a161a58ed2f431358a9f91fdb236a7ef9f44
Jared Forsyth <jared@jaredforsyth.com>
2013-06-17 17:48:35 -0600

    adding init

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ var template = require('./template');
 var keys = require('keys');
 
 module.exports = {
+  init: function (scope, element, attrs) {
+    element.find('input').bind('blur', function () {
+      var res = keys.normalize(this.value);
+      if (res.value) {
+        this.value = res.value;
+      }
+    });
+  },
   validator: function (scope, ctrl) {
     return function (value) {
-      if (keys.validate(value)) {
-        ctrl.$setValidity('shortcut', true);
-        return value;
-      } else {
+      var res = keys.normalize(value);
+      if (res.error) {
         ctrl.$setValidity('shortcut', false);
+      } else {
+        ctrl.$setValidity('shortcut', true);
       }
+      if (res.value) return value;
     }
+  },
+  normalize: function (value) {
+    return keys.normalize(value).value;
   },
   template: template,
 };
